84049288277dc11dd8c2f61b0bea4846bf7161d8
eric-sentient <eric-sentient@berkeley.edu>
2023-03-07 09:57:27 -0800

    updated with mui and njs static

---
+++ _next/static/amgDwo5uDMQAPnSg4Y0up/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/736-c8f75d36768f7713.js","static/chunks/pages/index-c6f89f95b9ac3d12.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
